7625cb30f0cec16f01038e10b22ba28bfa764918
neymaru <106804514+neymaru@users.noreply.github.com>
2023-03-14 23:49:17 +0900

    230314 코딩테스트

--- Programmers/Lv0/02.두수의차_ksh.js
+++ Programmers/Lv0/02.두수의차_ksh.js
@@ -3,5 +3,3 @@ function solution(num1, num2) {
 }
 
 console.log(solution(100, 2));
-
-
---
+++ Programmers/Lv0/23.배열두배만들기_ksh.js
@@ -0,0 +1,4 @@
+const solution = (numbers) => numbers.map((el) => el * 2);
+
+console.log(solution([1, 2, 3, 4, 5]));
+console.log(solution([1, 2, 100, -99, 1, 2, 3]));
