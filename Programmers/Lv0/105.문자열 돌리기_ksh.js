1a88dda8dc9fc63627ab0b7daff26c8138b711d0
neymaru <106804514+neymaru@users.noreply.github.com>
2023-05-09 23:52:43 +0900

    230509 코딩테스트 1문제

--- Programmers/Lv0/105.문자열 돌리기_ksh.js
+++ Programmers/Lv0/105.문자열 돌리기_ksh.js
@@ -10,5 +10,5 @@ rl.on("line", function (line) {
   input = [line];
 }).on("close", function () {
   str = input[0];
-  [...str].map((el) => console.log(el));
+  for(let i of str) console.log(i);
 });
