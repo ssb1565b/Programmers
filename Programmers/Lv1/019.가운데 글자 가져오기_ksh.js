39df1cdb70281826d683ff2f7eedab3dfd262088
neymaru <javaxkim@naver.com>
2023-06-01 22:32:08 +0900

    230601 코딩테스트 1문제

---
+++ Programmers/Lv1/019.가운데 글자 가져오기_ksh.js
@@ -0,0 +1,8 @@
+function solution(s) {
+  return s.length % 2
+    ? s[Math.floor(s.length / 2)]
+    : s.slice(s.length / 2 - 1, s.length / 2 + 1);
+}
+
+console.log(solution("abcde"));
+console.log(solution("qwer"));
