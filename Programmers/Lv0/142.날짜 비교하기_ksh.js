e926d49b3133e7cce21c9911df74a794eb547650
neymaru <javaxkim@naver.com>
2023-06-04 23:13:40 +0900

    230604 코딩테스트 1문제

---
+++ Programmers/Lv0/142.날짜 비교하기_ksh.js
@@ -0,0 +1,4 @@
+const solution = (date1, date2) => (new Date(date1) < new Date(date2) ? 1 : 0);
+
+console.log(solution([2021, 12, 28], [2021, 12, 29]));
+console.log(solution([1024, 10, 24], [1024, 10, 24]));
