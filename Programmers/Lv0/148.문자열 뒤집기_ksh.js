ce4aa379961560b02bc923cc6d931c7c92a2490d
neymaru <javaxkim@naver.com>
2023-06-12 23:59:44 +0900

    230612 코딩테스트 1문제

--- Programmers/Lv0/148.문자열 뒤집기_ksh.js
+++ Programmers/Lv0/148.문자열 뒤집기_ksh.js
@@ -1,8 +1,7 @@
 function solution(my_string, s, e) {
   return (
     my_string.substr(0, s) +
-    [...my_string.substr(s, e - s + 1)].reverse().join("") +
-    my_string.substr(e + 1)
+    [...my_string.substr(s, e - s + 1)].reverse().join("")
   );
 }
 
