2fe2190fc31e8900bafd03f8d5a776439f8eabe1
김성현 <106804514+neymaru@users.noreply.github.com>
2023-07-05 23:44:36 +0900

    230705 코딩테스트 1문제

---
+++ Programmers/Lv0/165.문자열 계산하기_ksh.js
@@ -0,0 +1,12 @@
+function solution(strArr) {
+  const obj = {};
+  strArr.forEach((el) => {
+    const len = el.length;
+    obj[len] = obj[len] || [];
+    obj[len].push(el);
+  });
+  const arr = Object.values(obj).map((el) => el.length);
+  return Math.max(...arr);
+}
+
+console.log(solution(["a", "bc", "d", "efg", "hi"]));
