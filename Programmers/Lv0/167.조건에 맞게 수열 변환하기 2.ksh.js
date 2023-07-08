e873dceabb2455a0747378c60ce5c2ebbec8954f
김성현 <106804514+neymaru@users.noreply.github.com>
2023-07-09 00:50:31 +0900

    코딩테스트 1문제 수정

--- Programmers/Lv0/167.조건에 맞게 수열 변환하기 2.ksh.js
+++ Programmers/Lv0/167.조건에 맞게 수열 변환하기 2.ksh.js
@@ -1,13 +1,16 @@
 function solution(arr) {
+  arr2 = [arr];
   let cnt = 0;
-  while (cnt > 10) {
-    arr.forEach((el) => {
-      cnt += 1;
-      el <= 50 ? (el % 2 ? el * 2 + 1 : el * 2) : el;
-      if (tempArr[cnt].join("") === tempArr.join("")) {
-        return cnt - 1;
-      }
-    });
+  while (true) {
+    cnt += 1;
+    arr2.push(
+      arr2[arr2.length - 1].map((el) =>
+        el >= 50 ? (el % 2 === 0 ? el / 2 : el) : el % 2 ? el * 2 + 1 : el
+      )
+    );
+    if (arr2[cnt].join("") === arr2[cnt - 1].join("")) {
+      return cnt - 1;
+    }
   }
 }
 
