6e2ae513d310f8e7f432f7f2945cc4547f01deac
김성현 <106804514+neymaru@users.noreply.github.com>
2023-08-03 23:55:47 +0900

    230803 코딩테스트 1문제

---
+++ Programmers/Lv1/036.이상한 문자 만들기_ksh.js
@@ -0,0 +1,20 @@
+function solution(s) {
+  let answer = "";
+  let words = s.split(" ");
+
+  for (let i = 0; i < words.length; i += 1) {
+    for (let j = 0; j < words[i].length; j += 1) {
+      if (j % 2 === 0) {
+        answer += words[i][j].toUpperCase();
+      } else {
+        answer += words[i][j].toLowerCase();
+      }
+    }
+    if (i < words.length - 1) {
+      answer += " ";
+    }
+  }
+  return answer;
+}
+
+console.log(solution("try hello world"));
